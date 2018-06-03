00b9449912a0d80ec9475f6ba70e303968d252a8
ThuyNT13 <ngo.thuy38@gmail.com>
2018-06-03 01:29:42 -0700

    Add test for head title; pass test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def head_title(page_title="")
+    base_title = "RailsApp"
+
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,9 +2,14 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = "RailsApp"
+  end
+
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select 'title', "Home | #{@base_title}"
   end
 
 end
