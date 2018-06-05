b368bd86816e3064e213564750987b558e8d7a11
ThuyNT13 <ngo.thuy38@gmail.com>
2018-06-04 23:39:28 -0700

    Add Session logout and test; pass test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,7 +6,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:user1)
   end
 
-  test "login with valid information" do
+  test "valid login" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { username: @user.username, password: "password" } }
@@ -19,7 +19,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
   end
 
-  test "login with invalid information" do
+  test "invalid login" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
@@ -27,5 +27,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "valid login followed by logout" do
+    get login_path
+    post login_path, params: { session: {
+      username: @user.username,
+      password: 'password'
+      }}
+    assert is_logged_in?
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
