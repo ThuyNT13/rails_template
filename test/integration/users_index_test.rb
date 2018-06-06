78ac545cd3eb0bd5ee5c661e72cb57a3a8056f2e
ThuyNT13 <ngo.thuy38@gmail.com>
2018-06-06 00:16:49 -0700

    Add more assertions to users_index_text.rb; pass tests

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,12 +6,23 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @user = users(:user1)
   end
 
-  test 'should get users list with login' do
+  test 'should get users list when logged in' do
     login_as(@user)
     get users_path
     assert_template 'users/index'
     assert_select "title",  head_title("Members")
     assert_response :success
+  end
+
+  test 'should not get users list when not logged in' do
+    get users_path
+    assert_not is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to login_url
+    follow_redirect!
+    assert_template 'sessions/new'
+    assert_select "title", head_title("Login")
+    assert_select 'form[action=?]', '/login'
   end
 
 end
